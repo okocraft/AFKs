79b3eac0195ff858ddb0e4891b391dc2b2cf4b7a
Siroshun09 <siro.shun8@gmail.com>
2020-04-13 18:00:47 +0900

    [Update] 判定時間の正確化とメソッド分けなどによるコードの整理
    - BukkitRunnable に記述されていた処理をいくつかのメソッドに分け、読みやすくした
    - AFK チェックと kick までの時間の設定を分けることで、 離席時間の誤差を小さくした
    - Java の Scheduler を使うことで tps に影響されないようにし、さらに 1tick あたり 1player の判定で極力鯖への影響を小さくした

--- src/main/java/net/okocraft/afks/AFKs.java
+++ src/main/java/net/okocraft/afks/AFKs.java
@@ -1,46 +1,76 @@
 package net.okocraft.afks;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class AFKs extends JavaPlugin {
 
-	private Map<Player, Location> previousRotation = new HashMap<>();
+    private final Map<Player, Location> previousRotation = new HashMap<>();
+    private final Map<Player, Long> lastActionTime = new HashMap<>();
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private long kickPeriod;
 
-	@Override
-	public void onEnable() {
-		saveDefaultConfig();
-		new BukkitRunnable(){
-	
-			@Override
-			public void run() {
-				Bukkit.getOnlinePlayers().forEach(player -> {
-					if (player.hasPermission("afks.bypass.command")) {
-						return;
-					}
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        kickPeriod = getConfig().getLong("time-to-kick", 300) * 1000;
+        scheduler.scheduleAtFixedRate(this::runCheckTask, 1L, getConfig().getLong("afk-check-task-period", 60), TimeUnit.SECONDS);
+    }
 
-					Location location = previousRotation.get(player);
-					if (location != null) {
-						if (player.getLocation().getYaw() == location.getYaw()
-								&& player.getLocation().getPitch() == location.getPitch()) {
-							getConfig().getStringList("command-for-afk")
-									.forEach(command -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("%player%", player.getName())));
-						}
-					}
-	
-					previousRotation.put(player, player.getLocation());
-				});
-			}
-		}.runTaskTimer(this, 1L, getConfig().getInt("afk-check-task-priod", 6000));
-	}
+    @Override
+    public void onDisable() {
+        getServer().getScheduler().cancelTasks(this);
+        scheduler.shutdownNow();
+    }
 
-	@Override
-	public void onDisable() {
-	}
+    public void runCheckTask() {
+        int i = 0;
+        for (Player player : getServer().getOnlinePlayers()) {
+            i++;
+            getServer().getScheduler().runTaskLater(this, () -> check(player), i);
+        }
+    }
+
+    private void check(Player player) {
+        if (player.hasPermission("afks.bypass.command")) {
+            return;
+        }
+
+        Location prev = previousRotation.get(player);
+        Location now = player.getLocation().clone();
+
+        if (prev != null) {
+            if (prev.getPitch() == now.getPitch() && prev.getYaw() == now.getYaw()) {
+                if (checkAFKTime(player)) {
+                    kickAction(player);
+                }
+                return;
+            }
+        }
+        previousRotation.put(player, now);
+        lastActionTime.put(player, System.currentTimeMillis());
+    }
+
+    private boolean checkAFKTime(Player player) {
+        Long prevTime = lastActionTime.get(player);
+        if (prevTime == null) {
+            lastActionTime.put(player, System.currentTimeMillis());
+            return false;
+        } else {
+            return kickPeriod < System.currentTimeMillis() - prevTime;
+        }
+    }
+
+    private void kickAction(Player player) {
+        for (String cmd : getConfig().getStringList("command-for-afk")) {
+            getServer().dispatchCommand(getServer().getConsoleSender(), cmd.replaceAll("%player%", player.getName()));
+        }
+    }
 }
